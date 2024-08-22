18b51b33d3442448ad173c0b097aee320b258d86
aojicong@megvii.com <jicong.ao@tum.de>
2024-08-22 16:40:25 +0800

    add audio client

--- BBAudioTranscriber/audio_client.py
+++ BBAudioTranscriber/audio_client.py
@@ -1,10 +1,10 @@
 import requests
 import io
 
-## BB 
-from keyboard_record import record_voice
+# BB 
+from .keyboard_record import record_voice
 
-def send_voice_request(url = "http://127.0.0.1:8001/transcribe"):
+def send_voice_request(url = "http://127.0.0.1:8001/transcribe") -> str:
     '''
     send the voice in Bytes to the server and get the transcription with the key "what"
     return None if the request is not successful (200)
@@ -39,7 +39,70 @@ def send_request(url):
     else:
         return None
     
+
+class AudioClient:
+    '''
+    An audio client to send voice request to the server
+    It expects a return of the transcription with the key "what"
+    '''
+    def __init__(self, protocol = "http", url = "127.0.0.1", port = 8001, endpoint = "transcribe"):
+        self.protocol = protocol
+        self.url = url
+        self.port = port
+        self.endpoint = endpoint
+
+    def send_voice_request(self) -> str:
+        '''
+        send the voice in Bytes to the server and get the transcription with the key "what"
+        return str if the request is successful (200)
+        '''
+        # record and convert audio to bytes
+        audio_bytes = record_voice().tobytes()
+
+        # prepare request
+        recording = {"voice": io.BytesIO(audio_bytes)}
+
+        try:
+            response = requests.post(self.get_full_url(), files=recording, timeout=20)
+        except requests.exceptions.Timeout:
+            raise Exception(f'Request timed out!')
+        
+        if response.status_code == 200:
+            data = response.json()
+            transcription = data["what"]
+            return transcription
+        else:
+            raise Exception(f'Request failed with status code {response.status_code}')
+        
+    def send_request(self):
+        '''
+        a test function to send a request to the server
+        currently not in use because the endpoint for testing is ceased
+        '''
+        try:
+            response = requests.post(self.get_full_url(), timeout=10)
+        except requests.exceptions.Timeout:
+            raise Exception(f'Request timed out!')
+        if response.status_code == 200:
+            data = response.json()
+            transcription = data["what"]
+            print(transcription)
+            return transcription
+        else:
+            raise Exception(f'Request failed with status code {response.status_code}')
+        
+    def set_connection(self, protocol, url, port, endpoint):
+        self.protocol = protocol
+        self.url = url
+        self.port = port
+        self.endpoint = endpoint
+
+    def get_full_url(self) -> str:
+        return f"{self.protocol}://{self.url}:{self.port}/{self.endpoint}"
+
+    
 if __name__ == "__main__":
-    url = "http://localhost:8001/transcribe"
-    # transcription = send_request(url)
-    print(send_voice_request())
+    
+    AT = AudioClient(protocol="http", url="127.0.0.1", port=8001, endpoint="transcribe")
+    transcription = AT.send_voice_request()
+    print(transcription)
--- setup.py
+++ setup.py
@@ -9,6 +9,15 @@ setup(
     author_email="jicong.ao@tum.de",
     license="MIT",
     packages=["BBAudioTranscriber"],
+    install_requires=[
+        "numpy<2",
+        "scipy",
+        # "fastapi[standard]",
+        # "transformers",
+        "sounddevice",
+        "pydub",
+        "pynput",
+    ],
     # duplicated in requirements.txt
     zip_safe=False,
 )
